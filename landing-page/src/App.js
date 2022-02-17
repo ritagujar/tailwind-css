78d90a13d00d50c304d50ac275e496361eff7b25
unknown <ritagujar>
2022-02-17 09:04:02 +0530

    Tailwind Setup

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
-
 function App() {
   return (
-    <h1>Hello World</h1>
-  );
+  <div>  <h1 className="text-3xl font-bold underline text-red-600">
+  Hello world!
+</h1></div>
+  )
 }
 
 export default App;
