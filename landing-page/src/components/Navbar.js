15c29e4afa69c3bb11daf95cc7927ac38919f3a7
759153 <ritagujar14@gmail.com>
2022-04-03 18:00:57 +0530

    Added Hamburger Menu

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="flex justify-between items-center py-5 px-14 ">
-      <div className="text-center justify-start">
-        <h1 className=" text-white cursor-pointer text-xl hover:text-green-300 ">
+      <div className="items-center">
+        <h1 className=" text-white cursor-pointer text-center text-xl hover:text-green-300">
           a.o
         </h1>
       </div>
-      <ul className="flex text-gray-500 text-lg justify-center list-none">
+      <ul className=" text-gray-500 text-lg text-center list-none hidden lg:flex">
         <li className="mr-6  hover:text-green-300 ">
           <a href="/" className="tracking-wide">
             About
@@ -40,7 +43,7 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
-      <div className="flex text-center">
+      <div className="items-center lg:flex  hidden ">
         <li className="list-none">
           <a
             href="/"
@@ -52,6 +55,70 @@ const Navbar = () => {
         <button className="ml-8 bg-buttonGreen h-9 w-24 font-semibold hover:bg-green-300 tracking-wide">
           Get Stated
         </button>
+      </div>
+      <div className="lg:hidden ml-4 relative">
+        <RiMenu3Line
+          color="#fff"
+          size={27}
+          onClick={() => setToggleMenu(true)}
+          className="cursor-pointer"
+        />
+
+        {toggleMenu && (
+          <div className="flex fixed left-0 top-0  w-full  bg-white">
+            <RiCloseLine
+              color="#000"
+              size={45}
+              onClick={() => setToggleMenu(false)}
+              className="cursor-pointer absolute top-7 right-12"
+            />
+            <ul className="flex text-gray-500 text-lg list-none flex-col  ">
+              <li className="mr-6  hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide ">
+                  About
+                </a>
+              </li>
+              <li className="mr-6 hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide">
+                  Product
+                </a>
+              </li>
+              <li className="mr-6 hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide">
+                  Customer
+                </a>
+              </li>
+              <li className="mr-6 hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide">
+                  Prices
+                </a>
+              </li>
+              <li className="mr-6 hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide">
+                  Our team
+                </a>
+              </li>
+              <li className="mr-6 hover:text-green-300 text-center">
+                <a href="/" className="tracking-wide">
+                  Blog
+                </a>
+              </li>
+            </ul>
+            {/* <div className="flex items-center">
+              <li className="list-none">
+                <a
+                  href="/"
+                  className="text-buttonGreen text-lg font-semibold hover:text-green-300 tracking-wide"
+                >
+                  Log in
+                </a>
+              </li>
+              <button className="ml-8 bg-buttonGreen h-9 w-24 font-medium hover:bg-green-300 tracking-wide">
+                Get Stated
+              </button>
+            </div> */}
+          </div>
+        )}
       </div>
     </div>
   );
