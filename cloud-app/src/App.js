86d7b1a5517b0c078afe01c9cb4655e18c5c2f82
759153 <ritagujar14@gmail.com>
2022-04-04 22:07:18 +0530

    Added About and Support Section

--- cloud-app/src/App.js
+++ cloud-app/src/App.js
@@ -1,11 +1,15 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Support from "./components/Support";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Support />
     </>
   );
 }
